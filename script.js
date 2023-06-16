00e2af60fbf589772a1da3b839ec068994b63fcb
Dikesh Kumar <117905520+Dikesh01@users.noreply.github.com>
2023-06-17 00:44:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 // //complete this code
+// 	class Animal {
+// 	constructor(someSpecies){
+// 		this._species = someSpecies;
+// 	}
+// 	get species(){
+// 	   return this._species;
+// 	}
+//     makeSound(){
+// 		console.log(`The ${this.species} makes a sound`);
+// 	}
+// }
+ 
+// class Dog extends Animal {
+// 	bark(){
+// 		console.log("woof");
+// 	}
+// }
+ 
+// class Cat extends Animal {
+// 	purr(){
+// 		console.log("purr");
+// 	}
+// }
+ 
+// // Do not change the code below this line
+// window.Animal = Animal;
+// window.Dog = Dog;
+// window.Cat = Cat;
+
 	class Animal {
+    species;
 	constructor(someSpecies){
-		this._species = someSpecies;
+		this.species = someSpecies;
 	}
 	get species(){
-	   return this._species;
+	   return this.species;
 	}
     makeSound(){
 		console.log(`The ${this.species} makes a sound`);
@@ -27,35 +57,3 @@ class Cat extends Animal {
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
-
-// class Animal {
-// 	constructor(someSpecies){
-// 		this.species = someSpecies;
-// 	}
-// 	get species(){
-// 		return this.species;
-// 	}
-// 	makeSound(){
-// 		console.log(`The ${this.name} makes a sound`);
-// 	}
-// }
-
-// class Dog extends Animal {
-	
-// 	bark(){
-// 		console.log("woof");
-// 	}
-
-// }
-
-// class Cat extends Animal {
-	
-// 	purr(){
-// 		console.log("purr");
-// 	}
-// }
-
-// // Do not change the code below this line
-// window.Animal = Animal;
-// window.Dog = Dog;
-// window.Cat = Cat;
